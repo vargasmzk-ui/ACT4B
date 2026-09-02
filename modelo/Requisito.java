f3da35001acd1c2e2d8665d0850b921dea4404a6
vargasmzk-ui <vargas.mzk@gmail.com>
2026-09-02 16:54:18 +0000

    GUARDAR

--- modelo/Requisito.java
+++ modelo/Requisito.java
@@ -1,5 +1,21 @@
-package modelo;
-
 public class Requisito {
-    
+   
+    // Atributos
+    private String nombreDocumento;
+    private boolean fueEntregado;
+
+    // Constructor
+    public Requisito (String nombreDocumento, boolean fueEntregado) {
+        this.nombreDocumento = nombreDocumento;
+        this.fueEntregado = false; // Por defecto, inicia sin entregarse.
+    }
+
+    // Método - Requisito
+    public void marcarComoEntregado() {
+        this.fueEntregado = true;
+        System.out.println("\nLos documentos [" + nombreDocumento + "] han sido recibidos.");
+    }
+
+    // Método Getter
+    public boolean getFueEntregado() { return fueEntregado; }
 }
